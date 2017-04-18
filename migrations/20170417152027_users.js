38d10236f69a03c1c2aebc1e9637ad04825b61cd
jenniferwiner <jeniwiner@gmail.com>
2017-04-17 20:13:06 -0600

    Schema created, db migrate and seed

--- migrations/20170417152027_users.js
+++ migrations/20170417152027_users.js
@@ -1,7 +1,8 @@
 exports.up = function(knex) {
   return knex.schema.createTable('users', (table) => {
     table.increments()
-    // stuff here
+    table.text('name').notNullable().defaultTo('')
+    table.text('email').notNullable().defaultTo('')
   })
 }
 
--- seeds/1_users.js
+++ seeds/1_users.js
@@ -3,19 +3,11 @@ exports.seed = function(knex) {
   return knex('users').del()
     .then(function() {
       // Inserts seed entries
-      // DECIDE ON SCHEMA, placeholders below
       return knex('users').insert([
         {
           id: 1,
-          colName: 'rowValue1'
-        },
-        {
-          id: 2,
-          colName: 'rowValue2'
-        },
-        {
-          id: 3,
-          colName: 'rowValue3'
+          name: 'Jennifer Winer',
+          email: 'jeniwiner@gmail.com'
         }])
     })
     .then(() => {
